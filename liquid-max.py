5ce9f99a5bf0a3c61b14d53445db1cf4db718d1c
soup-tech <campbellssoup21@gmail.com>
2021-01-02 17:23:26 -0500

    liquid-max 0.2

--- liquid-max.py
+++ liquid-max.py
@@ -14,6 +14,18 @@ GUILD = os.getenv('DISCORD_GUILD')
 
 bot = commands.Bot(command_prefix='!')
 
+def maximillian():
+    inputFile = open('maximillian.txt','r')
+    myList = []
+
+    for line in inputFile:
+        myList.append(line.strip())
+
+    msg = random.choice(myList)
+
+    inputFile.close()
+    return msg
+
 @bot.event
 async def on_ready():
     print('Logged in as')
@@ -23,11 +35,19 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
-    print('wake up')
     if (message.content == 'wake up'):
         await message.channel.send(f'Wake up <@792591005525737492>...')
+
+    await bot.process_commands(message)
+
+
+@bot.command(name='liquid-max')
+async def liquid_max(ctx):
+
+    if (ctx.author.id == 792591005525737492):
+        msg = maximillian()
+
+    await ctx.send(msg)
     
-    if (message.content == f'<@793190489788645466>'):
-        await message.channel.send('control is an illusion')
 
 bot.run(TOKEN)
